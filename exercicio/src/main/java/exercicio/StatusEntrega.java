6af8702669adaca792441f6b062b7f95a866300e
RENAN SOUSA LOPES FERRAZ <36793899+lopesti@users.noreply.github.com>
2026-04-23 22:23:36 -0300

    atualizei esse arquivo

--- exercicio/src/main/java/exercicio/StatusEntrega.java
+++ exercicio/src/main/java/exercicio/StatusEntrega.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exercicio;
 
-/**
- *
- * @author Admin
- */
+
 public enum StatusEntrega {
     PENDENTE,
     EM_TRANSITO,
